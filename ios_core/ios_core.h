852728d5ea1654b4c1734676f8403f84b74c7594
Petr Mares <scientica@petr-mac-mini.local>
2017-05-04 16:20:57 +0200

    Initial Commit

---
+++ ios_core/ios_core.h
@@ -0,0 +1,19 @@
+//
+//  ios_core.h
+//  ios_core
+//
+//  Created by Petr Mares on 04.05.17.
+//  Copyright © 2017 Scientica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios_core.
+FOUNDATION_EXPORT double ios_coreVersionNumber;
+
+//! Project version string for ios_core.
+FOUNDATION_EXPORT const unsigned char ios_coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_core/PublicHeader.h>
+
+
