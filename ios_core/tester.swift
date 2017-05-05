8a990054d9c6a20c89cfab738f5de90366d5b3f9
Scientica Agency <scientica@petr-mac-mini.local>
2017-05-05 10:28:40 +0200

    init core

---
+++ ios_core/tester.swift
@@ -0,0 +1,18 @@
+//
+//  tester.swift
+//  ios_core
+//
+//  Created by Petr Mares on 04.05.17.
+//  Copyright © 2017 Scientica. All rights reserved.
+//
+
+import Foundation
+public class Tester{
+    public init(){
+        
+    }
+    
+    public func getHello() -> String {
+        return "Hello"
+    }
+}
